10cf9f901bee8076a334f38ae8eff66d16e67f18
Amanda R Fernandes <amandadcunha@gmail.com>
2018-10-08 06:42:37 -0400

    Updates

---
+++ precache-manifest.2d8e1d405a6d0348d0a5b9f324f8a01d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a7192d623ea34590ffe",
+    "url": "/react-github-card/static/js/runtime~main.0a7192d6.js"
+  },
+  {
+    "revision": "cb85991930bae3d07341",
+    "url": "/react-github-card/static/js/main.cb859919.chunk.js"
+  },
+  {
+    "revision": "8489fcf1eea22f270e0b",
+    "url": "/react-github-card/static/js/1.8489fcf1.chunk.js"
+  },
+  {
+    "revision": "cb85991930bae3d07341",
+    "url": "/react-github-card/static/css/main.67161c11.chunk.css"
+  },
+  {
+    "revision": "c588279c9c7774d87d40daf049fa6ae7",
+    "url": "/react-github-card/index.html"
+  }
+];
